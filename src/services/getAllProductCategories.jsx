f4b7fe8bfc258bf831d379b62eb81212fa03fc49
nicoghaza <nicoghza@gmail.com>
2024-11-18 18:09:34 +0700

    first commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,15 @@
+const getAllProductCategories = ()  => {
+    return [   {
+      id: 'FFutsaL',
+      slug: 'futsal-shoes',
+      name: `Futsal's Shoes`
+    },
+    {
+      id: 'FRUNNING',
+      slug: 'running-shoes',
+      name: `Running's Shoes`
+    },
+    ]
+  }
+  
+  export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,93 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'ORT-Ortuseight FORTE METEOR IN Black Silver-001',
+      slug: 'Ortuseight FORTE METEOR IN Black Silver-futsal',
+      name: "Ortuseight FORTE METEOR IN  'Black Silver",
+      category: `Futsal's Shoes`,
+      categorySlug: 'futsal-shoes',
+      price: 479_000,
+      stock: 100,
+      imageUrl: '/assets/images/Ortuseight FORTE METEOR IN Black Silver.jpg',
+      description: `FORTE METEOR IN adalah sepatu futsal dengan teknologi Quick Fit pada upper, Cumulus foam dan Ortshox pada midsole untuk kenyamanan maksimal, serta OrtCurve pada outsole untuk traksi optimal. Terbuat dari upper PU yang tahan lama, midsole Inj. Phylon ringan, dan outsole rubber 55 untuk stabilitas. Tingkatkan performa futsal Anda dengan FORTE METEOR IN!`
+    },
+    {
+      id: 'ORT-Ortuseight JOGOSALA CONQUER Ocean Blue-002',
+      slug: 'Ortuseight JOGOSALA CONQUER Ocean Blue-futsal',
+      name: "Ortuseight JOGOSALA CONQUER   'Ocean Blue",
+      category: `Futsal's Shoes`,
+      categorySlug: 'futsal-shoes',
+      price: 549_800,
+      stock: 5,
+      imageUrl: `/assets/images/Ortuseight JOGOSALA CONQUER Ocean Blue.jpg`,
+      description: `JOGOSALA CONQUER adalah produk terbaru yang hadir dari Ortuseight. Produk ini dirilis pada musim panas 2024. Jogosala Conquer dilengkapi dengan teknologi terbaru seperti Upper Quick Fit, midsole Cumulus foam dan Ortshox, serta outsole Ortcurve. Material pada bagian atasnya terbuat dari Splite suede leather + sandwich mesh, material pada midsole menggunakan injection phylon, dan material pada outsole menggunakan karet.`
+    },
+    {
+      id: 'ORT-Ortuseight HAVOC IN Navy White Pink-003',
+      slug: 'Ortuseight HAVOC IN Navy White Pink-futsal',
+      name: "Ortuseight HAVOC IN  'Navy White Pink",
+      category: `Futsal's Shoes`,
+      categorySlug: 'futsal-shoes',
+      price: 429_000,
+      stock: 0,
+      imageUrl: `/assets/images/Ortuseight HAVOC IN Navy White Pink.jpg`,
+      description: `Rasakan dinamika permainan futsal yang tak terlupakan dengan HAVOC IN dari Ortuseight. Desain khusus untuk futsal, HAVOC IN menggabungkan teknologi canggih dan material pilihan untuk kinerja maksimal. Upper dari PU Synthetic dengan teknologi Quick Fit menyesuaikan dengan kaki Anda secara instan, sementara midsole Inj. Phylon dilengkapi dengan Cumulus Foam dan Ortshox untuk dukungan dan kenyamanan optimal. Dengan outsole OrtCurve, setiap langkah dan gerakan Anda dijamin stabil dan gesit. Pilih dari tiga pilihan warna menarik untuk menambah gaya Anda di lapangan.`
+    },
+    {
+      id: 'ORT-Ortuseight OLYMPICO V3 IN Off White Gold-004',
+      slug: 'Ortuseight OLYMPICO V3 IN Off White Gold-futsal',
+      name: "Ortuseight OLYMPICO V3 IN   'Off White Gold",
+      category: `Futsal's Shoes`,
+      categorySlug: 'futsal-shoes',
+      price: 449_000,
+      stock: 100,
+      imageUrl: '/assets/images/Ortuseight OLYMPICO V3 IN Off White Gold.jpg',
+      description: `OLYMPICO V3 IN adalah sepatu futsal terbaru dengan kenyamanan dan kinerja terbaik. Dilengkapi teknologi Quick Fit pada upper, Cumulus Foam dan Ortshox pada midsole, serta OrtCurve pada outsole, sepatu ini mendukung setiap langkah di lapangan. Dibuat dari bahan PU yang tahan lama, midsole Inj. Phylon untuk amortisasi maksimal, dan outsole dengan traksi optimal. Tingkatkan performa futsal Anda dengan OLYMPICO V3 IN!`
+    },
+    {
+      id: 'ORT-Ortuseight HYPERFUSE 1.4 Peach White-005',
+      slug: 'Ortuseight HYPERFUSE 1.4 Peach White-running',
+      name: "Ortuseight HYPERFUSE 1.4  'Peach White",
+      category: `Running's Shoes`,
+      categorySlug: 'running-shoes',
+      price: 699_000,
+      stock: 5,
+      imageUrl: `/assets/images/Ortuseight HYPERFUSE 1.4 Peach White.jpg`,
+      description: `HYPERFUSE 1.4 dirancang untuk pengalaman lari nyaman dan jarak tempuh lebih jauh. Sepatu ini memiliki Cushion Midsole, berat hanya 250 gram, dan heel-to-toe drop 6 mm (heel stack 30,5 mm, forefoot stack 24,5 mm) untuk bantalan optimal. Cocok untuk Easy Run, Recovery Run, Tempo Run, dan Long Run, sepatu ini stabil untuk pelari netral. Bagian atas menggunakan teknologi Ortflow, Jaquards mesh, dan PU Nosew untuk kenyamanan dan sirkulasi udara. Midsole Cumulus OAM dan Ortshox menyerap goncangan, dengan Compression Phylon untuk bantalan ekstra. Outsole karet memberikan grip kuat di berbagai permukaan. Tersedia dalam 3 warna, HYPERFUSE 1.4 adalah pilihan ideal untuk lari yang halus dan nyaman.`
+    },
+    {
+      id: 'ORT-Ortuseight HYPERGLIDE 3.0 Castelion Green Neon-006',
+      slug: 'Ortuseight HYPERGLIDE 3.0 Castelion Green Neon-running',
+      name: "Ortuseight HYPERGLIDE 3.0  'Castelion Green Neon",
+      category: `Running's Shoes`,
+      categorySlug: 'running-shoes',
+      price: 699_000,
+      stock: 100,
+      imageUrl: '/assets/images/Ortuseight HYPERGLIDE 3.0 Castelion Green Neon.jpg',
+      description: `HYPERGLIDE 3.0 adalah sepatu lari generasi ketiga dengan desain ringan (243 gram) dan midsole Cumulus foam serta Ortshox untuk kecepatan dan bantalan optimal. Dengan heel-to-toe drop 6 mm, sepatu ini ideal untuk latihan harian dan lomba. Dilengkapi teknologi Ortflow dan Ortstrap pada bagian atas untuk kenyamanan dan sirkulasi udara, serta outsole karet untuk grip kuat. Tersedia dalam tiga warna, HYPERGLIDE 3.0 memberikan responsivitas tinggi dan performa maksimal.`
+    },
+    {
+      id: 'ORT-Ortuseight HYPERFUSE 2.1 Grey Lime Cyan-007',
+      slug: 'Ortuseight HYPERFUSE 2.1 Grey Lime Cyan-running',
+      name: "Ortuseight HYPERFUSE 2.1  'Grey Lime Cyan",
+      category: `Running's Shoes`,
+      categorySlug: 'running-shoes',
+      price: 649_000,
+      stock: 100,
+      imageUrl: '/assets/images/Ortuseight HYPERFUSE 2.1 Grey Lime Cyan.jpg',
+      description: `HYPERFUSE 2.1 adalah sepatu lari terbaru dengan teknologi Ortflow pada upper, Cumulus foam dan Ortshox pada midsole. Upper terbuat dari material Sandwich mesh dan PU Nosew yang ringan dan tahan lama. Midsole menggunakan compression Phylon dengan Ortholite insole untuk kenyamanan maksimal. Outsole berbahan rubber hardness 55, tahan lama dan cocok untuk berbagai cuaca. Dapatkan HYPERFUSE 2.1 dan rasakan kenyamanannya saat berlari.`
+    },
+    {
+      id: 'ORT-Ortuseight HYPERGLIDE 3.0 White Ortred Navy-008',
+      slug: 'Ortuseight HYPERGLIDE 3.0 White Ortred Navy-running',
+      name: "Ortuseight HYPERGLIDE 3.0  'White Ortred Navy",
+      category: `Running's Shoes`,
+      categorySlug: 'running-shoes',
+      price: 699_000,
+      stock: 0,
+      imageUrl: `/assets/images/Ortuseight HYPERGLIDE 3.0 White Ortred Navy.jpg`,
+      description: `Ortuseight HYPERGLIDE 3.0 White Ortred Navy adalah sepatu olahraga yang menggabungkan desain modern dengan performa maksimal. Memiliki warna dominan putih dengan aksen merah dan biru navy, sepatu ini menciptakan tampilan yang dinamis dan elegan. Dilengkapi dengan teknologi terbaru untuk kenyamanan dan daya tahan, HYPERGLIDE 3.0 dirancang untuk memberikan kestabilan optimal saat berolahraga atau beraktivitas sehari-hari. Material berkualitas tinggi menjadikannya ringan dan responsif, sementara sol luar yang grip-friendly memastikan pijakan yang aman di berbagai permukaan. Sepatu ini adalah pilihan tepat bagi Anda yang mencari perpaduan gaya dan fungsionalitas.`
+    },
+
+  ]
+}
